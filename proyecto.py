0d4879eac67db000e28fd643a42c49401f53d320
Leo9513 <leodavid_13@hotmail.es>
2022-02-25 10:25:53 -0500

    Se ordena codigo y se ovtiene sumatoria de proyecciones Norte y Este

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,5 @@
 #Programa para leer archivo excel y devolver valores como una lista
+
 import pandas as pd
 import math as m
 
@@ -25,17 +26,31 @@ class Topoutils():
         self.decimal_angle =  degree_part + minutes/60 + seconds /3600
         return self.decimal_angle
 
+    def decimal_angle_to_gms(self, decimal_angle, total_decimals =0):
+        minutes, degree_part = m.modf(decimal_angle)
+        seconds, minutes = m.modf(minutes*60)
 
-    def error_angular(self,suma_angulo,):
+        self.gms_angle= "{}° {}' {}'' ".format(int(degree_part),int(minutes),round(seconds,total_decimals))
+        return self.gms_angle
+
+    def suma_ang(self,angulos_list):
+        suma=0
+        for angulo in angulos_list:
+            angulo_decimal = self.gms_angle_to_decimals(angulo)
+            suma+=angulo_decimal
+        self.suma=suma
+        return self.suma
+
+    def error_angular(self,suma_angulo):
         sentido = float(input("digite '1' si la poligonal tiene angulos internos, digite '2' si son externos: \n"))
         if sentido==1:
                 angulos_i = suma_angulo
                 angulos_it = float((vertices-2)*180+360)
                 cierre = float(angulos_i-angulos_it)
         else:
-                angulos_e = suma_angulo
-                angulos_et = float((vertices+2)*180)
-                cierre = float(angulos_e-angulos_et)
+            angulos_e = suma_angulo
+            angulos_et = float((vertices+2)*180)
+            cierre = float(angulos_e-angulos_et)
         self.cierre=cierre
         return self.cierre   
     
@@ -47,13 +62,16 @@ class Topoutils():
         self.corr_error=corr_error
         return self.corr_error
 
-    
-    def decimal_angle_to_gms(self, decimal_angle, total_decimals =0):
-        minutes, degree_part = m.modf(decimal_angle)
-        seconds, minutes = m.modf(minutes*60)
-
-        self.gms_angle= "{}° {}' {}'' ".format(int(degree_part),int(minutes),round(seconds,total_decimals))
-        return self.gms_angle
+    def suma_and_corr_ang(self,angulos_list,corr_error):
+        suma_ang_corr=0
+        ang_corr_list=[]
+        for angulo in angulos_list:
+            angulo_decimal = self.gms_angle_to_decimals(angulo)
+            angulocorr=angulo_decimal+corr_error
+            ang_corr_list.append(angulocorr)
+            suma_ang_corr+=angulocorr
+        self.suma_and_corr_ang=[suma_ang_corr,ang_corr_list]
+        return self.suma_and_corr_ang
 
     def bearing_and_distance(self):
         x1=float(input('Digite la coordenada X1: '))
@@ -117,81 +135,84 @@ class Topoutils():
         self.azimut=[azimut,contAzimut]
 
         return self.azimut
+    def azimut_and_contra(self,azimut_inicial,ang_corr_list):
+        azimut=[]
+        contraAzimut=[azimut_inicial]
+        lista=0
+        for angulo in ang_corr_list:
+            while lista!=len(ang_corr_list):
+                azimut_ady= self.azimut_adyacente(contraAzimut[lista],angulo)
+                lista+=1
+                azimut.append(azimut_ady[0])
+                contraAzimut.append(azimut_ady[1])
+                break
+        self.azimut_corr=azimut[:-1]
+        return self.azimut_corr
+
     def proy(self,azimut,dist):
         proy_y=((m.cos(m.radians(azimut))))*dist
         proy_x=((m.sin(m.radians(azimut))))*dist
-        self.coord=[proy_x,proy_y]
+        self.coord=[proy_y,proy_x]
         return self.coord
+    
+    def coord_list(self,azimut_corr,dist_list):
+        coord_list=[]
+        centinela=0
+        for azimut in azimut_corr:
+            while centinela!=len(azimut_corr):
+                coord_px_py=self.proy(azimut,dist_list[centinela])
+                coord_list.append(coord_px_py)
+                centinela+=1
+                break
+        self.coord_list=coord_list
+        return coord_list
+    
+
+    def suma_coord(self,coord,azimut_corr):
+
+        sumacoord_x=0
+        centinela=0
+        index=0
+        for coordenada in coord:
+            while centinela!=len(azimut_corr):
+                sumacoord_x+=coordenada[index]
+                centinela+=1
+                break
+        
+        sumacoord_y=0
+        centinela=0
+        index=1
+        for coordenada in coord:
+            while centinela!=len(azimut_corr):
+                sumacoord_y+=coordenada[index]
+                centinela+=1
+                break
+
+        self.suma_coord=[sumacoord_x,sumacoord_y]
+        return self.suma_coord
+        
 
 def main():
     info_topo =Topoutils()
 
-    suma=0
-    for angulo in angulos_list:
-        angulo_decimal = info_topo.gms_angle_to_decimals(angulo)
-        suma+=angulo_decimal
+    suma_ang=info_topo.suma_ang(angulos_list)
 
-    error_ang=info_topo.error_angular(suma)
+    error_ang=info_topo.error_angular(suma_ang)
 
     corr_error=info_topo.correccion_error(error_ang)
   
-
-    suma_ang_corr=0
-    ang_corr_list=[]
-    for angulo in angulos_list:
-        angulo_decimal = info_topo.gms_angle_to_decimals(angulo)
-        angulocorr=angulo_decimal+corr_error
-        ang_corr_list.append(angulocorr)
-        suma_ang_corr+=angulocorr
+    suma_and_list_ang=info_topo.suma_and_corr_ang(angulos_list,corr_error)
     
-
-
-
     rumbo=info_topo.bearing_and_distance()
 
-    
     azimut_inicial=info_topo.azimut_ini(rumbo)
 
-    azimut=[]
-    contraAzimut=[azimut_inicial]
-    lista=0
-    for angulo in ang_corr_list:
-        while lista!=len(ang_corr_list):
-            azimut_ady= info_topo.azimut_adyacente(contraAzimut[lista],angulo)
-            lista+=1
-            azimut.append(azimut_ady[0])
-            contraAzimut.append(azimut_ady[1])
-            break
-
-    azimut_corr=azimut[:-1]
+    azimut_corr=info_topo.azimut_and_contra(azimut_inicial, suma_and_list_ang[1])
+ 
+    list_coord=info_topo.coord_list(azimut_corr,dist_list)
     
-    print(dist_list)
-    print(azimut_corr)
-
-    coord=[]
-    for azimut in azimut_corr:
-        for dist in dist_list:
-            coord_px_py=info_topo.proy(azimut,dist)
-            coord.append(coord_px_py)
-            break
-    print(coord)
-
-
+    suma_coord=info_topo.suma_coord(list_coord,azimut_corr)
 
     
-
-
-    
-    
-
-
-    
-
-
-    
-
-
-    
-
 if __name__ == main():
     main()
