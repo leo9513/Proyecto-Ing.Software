f961ff255dfc8fc8c482e9090f025457d674f70e
MateoTellezxd <dmtellezp@correo.udistrital.edu.co>
2022-03-04 16:55:58 -0500

    LoginFinal
    Se modifican label, por messagebox entre otras cosas,para simplificar el codigo, queda el login regisntrando,validando y habilitando la ventana de seleccion.

--- LoginNuevoMetodo.py
+++ LoginNuevoMetodo.py
@@ -8,14 +8,14 @@ import os
 def ventana_inicio():
     global ventanaprincipal
     ventanaprincipal=Tk()
-    ventanaprincipal.geometry("400x400")
+    ventanaprincipal.geometry("400x500")
     ventanaprincipal.title("BIENVENIDO A TOPOAPP")
     ventanaprincipal.iconbitmap(r'C:\Users\Personal\Desktop\TRABAJOFINALINGE\mt.ico')
 
     imagen = PhotoImage(file = r"C:\Users\Personal\Desktop\TRABAJOFINALINGE\topoga.png")
 
     ventanaprincipal = Label(image = imagen, text = "fondo")
-    ventanaprincipal.place(x = 0, y = 00, relwidth = 1, relheight = 1)
+    ventanaprincipal.place(x = 0, y = 0, relwidth = 1, relheight = 1)
         
     Label(text="INGRESO AL SISTEMA",bg="royal blue",fg="white",width="20",height="2",font=("Consolas",15)).pack()
     Label(text="\n").pack ()
@@ -34,7 +34,7 @@ def registro():
     global ventana1
     ventana1 = Toplevel(ventanaprincipal)
     ventana1.title("REGISTRO DE USUARIO")
-    ventana1.geometry("250x250")
+    ventana1.geometry("400x500")
  
     global nomusuario
     global contrasena
@@ -42,25 +42,27 @@ def registro():
     global ingresocontrasena
     nomusuario = StringVar() 
     contrasena = StringVar() 
-    Label(ventana1, text="INGRESE DATOS", bg="bisque").pack()
+    Label(ventana1, text="INGRESE LOS DATOS", bg="blue",font=("Consolas",15)).pack()
+    Label(ventana1, text="").pack()
     Label(ventana1, text="").pack()
     nombre1 = Label(ventana1, text="NOMBRE DEL USUARIO  ")
     nombre1.pack()
     ingresonombre = Entry(ventana1, textvariable=nomusuario) 
     ingresonombre.pack()
-    etiqueta_clave = Label(ventana1, text="CONTRASEÑA  ")
-    etiqueta_clave.pack()
+    Label(ventana1, text="").pack()
+    e_contrasena = Label(ventana1, text="CONTRASEÑA  ")
+    e_contrasena.pack()
     ingresocontrasena = Entry(ventana1, textvariable=contrasena, show='*') 
     ingresocontrasena.pack()
     Label(ventana1, text="").pack()
-    Button(ventana1, text="REGISTRARSE", width=10, height=1, bg="firebrick1", command = registro_usuario).pack() 
+    Button(ventana1, text="REGISTRAR", width=10, height=1, bg="firebrick1",font=("Consolas",12), command = registro_usuario).pack() 
 
 def login():
     global ventana_login
     ventana_login = Toplevel(ventanaprincipal)
     ventana_login.title("INGRESO A LA APP")
-    ventana_login.geometry("300x250")
-    Label(ventana_login, text="INGRESE EL USUARIO Y CONTRASEÑA",bg="bisque").pack()
+    ventana_login.geometry("400x500")
+    Label(ventana_login, text="INGRESE EL USUARIO Y CONTRASEÑA",bg="bisque",font=("Consolas",15)).pack()
     Label(ventana_login, text="").pack()
  
     global verificarusuario
@@ -77,11 +79,11 @@ def login():
     entrada_login_usuario.pack()
     Label(ventana_login, text="").pack()
     Label(ventana_login, text="").pack()
-    Label(ventana_login, text="CCONTRASEÑA:  ").pack()
+    Label(ventana_login, text="CONTRASEÑA:  ").pack()
     entrada_login_clave = Entry(ventana_login, textvariable=verificarcontrasena, show= '*')
     entrada_login_clave.pack()
     Label(ventana_login, text="").pack()
-    Button(ventana_login, text="INGRESAR", width=10, height=2, command = verificar).pack()
+    Button(ventana_login, text="INGRESAR", width=10, height=1,font=("Consolas",15), command = verificar).pack()
 
 
 
@@ -108,30 +110,26 @@ def verificar():
 def ingresoexitoso():
     global ventana_exito
     ventana_exito = Toplevel(ventana_login)
-    ventana_exito.title("EXITOSO")
     ventana_exito.geometry("400x400")
-    Label(ventana_exito, text="INGRESO  EXITOSO").pack()
-    Button(ventana_exito, text="OK").pack()
- 
-
- 
+    Label(ventana_exito,text="SELECCIONE EL TIPO DE LEVANTAMIENTO",bg="royal blue",fg="white",width="40",height="2",font=("Consolas",15)).pack()
+    Label(ventana_exito, text="").pack()
+    Label(ventana_exito, text="").pack()
+    Button(ventana_exito, text="LEVANTAMIENTO PLANIMETRICO", width=30, height=2).pack()
+    Label(ventana_exito, text="").pack()
+    Label(ventana_exito, text="").pack()
+    Label(ventana_exito, text="").pack()
+    Button(ventana_exito, text="LEVANTAMIENTO ALTIMETRICO", width=30, height=2).pack()
+    
+    #Button2.place(x=100,y=100)
 def contrasenamal():
-    global ventana_no_clave
-    ventana_no_clave = Toplevel(ventana_login)
-    ventana_no_clave.title("ERROR")
-    ventana_no_clave.geometry("400x400")
-    Label(ventana_no_clave, text="CONTRASEÑA O USUARIO INCCORECTA ").pack()
-    Button(ventana_no_clave, text="OK").pack()
+    messagebox.showinfo(title="¡UPS!",message="USUARIO O CONTRASEÑA INCORRECTA ")
+   
  
 
  
 def usuariomal():
-    global ventana_no_usuario
-    ventana_no_usuario = Toplevel(ventana_login)
-    ventana_no_usuario.title("ERROR")
-    ventana_no_usuario.geometry("150x100")
-    Label(ventana_no_usuario, text="USUARIO O CONTRASEÑA INCORRECTA").pack()
-    Button(ventana_no_usuario, text="OK").pack() 
+    messagebox.showinfo(title="¡UPS!",message="USUARIO O CONTRASEÑA INCORRECTA ")
+    
 
 
 
@@ -150,9 +148,8 @@ def registro_usuario():
  
     ingresonombre.delete(0, END)
     ingresocontrasena.delete(0, END)
- 
-    Label(ventana1, text="SE HA REGISTRADO CON EXITO", fg="blue", font=("calibri", 11)).pack()
- 
+    
+    messagebox.showinfo(title="REGISTRO USUARIO",message="EL USUARIO SE HA REGISTRADO CON EXITO.")
  
 ventana_inicio() 
  
