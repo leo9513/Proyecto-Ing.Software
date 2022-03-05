56902a47f8a85772fc1d1465a14e72bb2572da08
JuliaanRC <98624122+JuliaanRC@users.noreply.github.com>
2022-03-05 11:09:58 -0500

    Update nivelacion geometrica simple.py

--- nivelacion geometrica simple.py
+++ nivelacion geometrica simple.py
@@ -1,4 +1,10 @@
 import math as m
+import pandas as pd
 
-def cotas ():
-    
+inputfile = r"C:\Users\julia\OneDrive\Escritorio\PYTHON\Input\NIVELACION_GEOMETRICA_SIMPLE_BASE.xlsx "
+outputfile = r"C:\Users\julia\OneDrive\Escritorio\PYTHON\output"
+
+nivelacion_file = pd.read_excel(inputfile , sheet_name= "Hoja1", header = 0)
+#print(nivelacion_file.shape)
+#print(nivelacion_file.columns)
+#print(nivelacion_file["VI"])
